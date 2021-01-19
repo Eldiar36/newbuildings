3c230df34d893f3bf46871d25f8f3239f4c91dbd
eldiar36 <eldiarrd@gmail.com>
2021-01-20 05:08:42 +0600

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".home-slider").owlCarousel({
+        items: 1,
+        loop:true,
+        nav:true,
+        navText:true,
+        margin:20,
+    });
+});
+$(document).ready(function(){
+    $(".offer-slider").owlCarousel({
+        items: 4,
+        loop:true,
+        nav:true,
+        navText:true,
+        margin:0,
+    });
+});
